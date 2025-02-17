f528f32cb5ff4e2be69a676cb71e1e85ce4b0495
Prashant Singh <prashantsingh@Prashants-MacBook-Air.local>
2025-02-18 00:06:56 +0530

    register captain done

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const cors = require('cors');
 const app = express();
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
+const captainRoutes=require('./routes/captain.route')
 
 connectToDb();
 
 app.use(cors());
 app.use(express.json()); // Middleware to parse JSON
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded data
-app.use('/users', userRoutes); // Change to /users
+
 app.get('/', (req, res) => {
     res.send("Hello world");
 });
+
+app.use('/users', userRoutes); 
+app.use('/captains', userRoutes); 
 
 module.exports = app;
